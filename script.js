0223dc598aaab47ee1161107054ff95b74c4ad33
Hleb Makarevich <Stabik2014@gmail.com>
2023-02-18 20:47:52 +0100

    hw2

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let e = 10
+alert(e)
+
+let yearofIssue = 2007
+alert (yearofIssue);
+
+let username = `Brendan Eich`
+alert (username)
+
+let x = 10
+let y = 2
+alert (x+y)
+alert(x-y)
+alert(x/y)
+
+const result = 2 ** 5;
+alert(result)
+
+let a = 9 
+let b = 2
+alert(a%b)
+
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+
+let age = prompt( "Сколько вам лет?")
+alert (age)
+
